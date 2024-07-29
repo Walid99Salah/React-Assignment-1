afb2c2d3430995ece7381409da3f61183e17f58e
Walid99Salah <walidsalah87@gmail.com>
2024-07-29 19:42:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import Portfolio from "./Components/Portfolio/Portfolio.jsx";
 import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import Home from "./Components/Home/Home.jsx";
+import Notfound from "./Components/Notfound/Notfound.jsx";
 
 let routers = createBrowserRouter([
   { path: "", element: <Layout />, children: [
-      { index:true, element: <Home /> },
+      { index: true, element: <Home /> },
       { path: "about", element: <About /> },
       { path: "portfolio", element: <Portfolio /> },
       { path: "contact", element: <Contact /> },
+      { path: "*", element: <Notfound /> },
     ],
   },
 ]);
